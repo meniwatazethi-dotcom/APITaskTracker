8af370525d4662e0b795a32dc9fc7fac75a7bdc3
Monde Deyi <meniwatazethi@gmail.com>
2025-11-10 22:54:55 +0200

    configure middleware on the program files and added inmemorydb nuget package

--- APITaskTracker/Program.cs
+++ APITaskTracker/Program.cs
@@ -1,25 +1,54 @@
+using APITaskTracker.Data;
+using APITaskTracker.Data.Repository.Interface;
+using APITaskTracker.ErrorHandler;
+using Microsoft.EntityFrameworkCore;
+using TaskTracker.Data.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(static opt =>
+    opt.UseInMemoryDatabase("TaskDb"));
+
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddProblemDetails();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("SpaPolicy", policy =>
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestPropertiesAndHeaders |
+                            Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponsePropertiesAndHeaders;
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseHttpLogging();
+
+app.UseGlobalExceptionHandler();
+app.UseHttpLogging();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseCors("SpaPolicy");
 app.MapControllers();
 
 app.Run();
