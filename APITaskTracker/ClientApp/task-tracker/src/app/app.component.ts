30fd20554214c7c089c46e880a8f3fd2e8a21b17
admin <meniwatazethi@gmail.com>
2025-11-16 14:29:10 +0200

    clean files

--- APITaskTracker/ClientApp/task-tracker/src/app/app.component.ts
+++ APITaskTracker/ClientApp/task-tracker/src/app/app.component.ts
@@ -1,20 +1,44 @@
+// import { Component } from '@angular/core';
+// import { RouterLinkActive, RouterOutlet } from '@angular/router';
+// import { FormsModule } from '@angular/forms';
+// import { CommonModule, DatePipe } from '@angular/common';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet, CommonModule,
+//     RouterOutlet,
+//     RouterLinkActive,
+//     DatePipe,
+//     FormsModule],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'Task Tracker';
+//    today = new Date();
+// }
+
 import { Component } from '@angular/core';
-import { RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule,
+  imports: [
+    CommonModule,
     RouterOutlet,
+    RouterLink,   
     RouterLinkActive,
     DatePipe,
-    FormsModule],
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Task Tracker';
-   today = new Date();
+  today = new Date();
 }
--- APITaskTracker/ClientApp/task-tracker/src/app/app.config.ts
+++ APITaskTracker/ClientApp/task-tracker/src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter, Routes } from '@angular/router';
 import { TaskformComponent } from './taskform/taskform.component';
 import { TaskListComponent } from './task-list/task-list.component';
 
-
 const routes: Routes = [
   { path: '', redirectTo: 'tasks', pathMatch: 'full' },
   { path: 'tasks', component: TaskListComponent },
@@ -15,36 +14,3 @@ const routes: Routes = [
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
-
-
-
-
-
-// import {
-//   ApplicationConfig,
-//   provideBrowserGlobalErrorListeners,
-//   provideZonelessChangeDetection,
-// } from '@angular/core';
-// import { provideRouter, Routes } from '@angular/router';
-// import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-// import { provideHttpClient } from '@angular/common/http';
-// import { TaskList } from './task-list/task-list';
-// import { TaskForm } from './task-form/task-form';
-
-// const routes: Routes = [
-//   { path: '', redirectTo: 'tasks', pathMatch: 'full' },
-//   { path: 'tasks', component: TaskList },
-//   { path: 'tasks/add', component: TaskForm },
-//   { path: 'tasks/edit/:id', component: TaskForm },
-//   { path: '**', redirectTo: 'tasks' }
-// ];
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideBrowserGlobalErrorListeners(),
-//     provideZonelessChangeDetection(),
-//     provideRouter(routes),
-//     provideClientHydration(withEventReplay()),
-//     provideHttpClient()
-//   ]
-// };
